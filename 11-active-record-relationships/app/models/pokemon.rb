1e4bb58ba831cdc0b7e870ea3313ffe78684354d
matt-mcalister <matt.mcalister93@gmail.com>
2018-12-21 15:56:58 -0500

    removed unnecessary code in 11

--- 11-active-record-relationships/app/models/pokemon.rb
+++ 11-active-record-relationships/app/models/pokemon.rb
@@ -1,16 +1,9 @@
 class Pokemon < ActiveRecord::Base
-  has_many(:trainers, {through: :poke_balls})
-  has_many(:poke_balls)
+  has_many :trainers,  through: :poke_balls 
+  has_many :poke_balls 
 
   def fight
     "#{name} attacks with #{attack}!!!!"
   end
-  #
-  # def trainer
-  #   Trainer.find(self.trainer_id)
-  # end
-  # def self.create(args)
-  #   poke = self.new(args)
-  #   poke.save
-  # end
+
 end
