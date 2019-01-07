24eafd6f6dafc6c9810fcf0022e542cd631c33cd
Maxcell Wilson <maxcell.wilson@gmail.com>
2019-01-07 12:31:24 -0500

    Update to include sinatra code

---
+++ 12-sinatra-intro/server.rb
@@ -0,0 +1,10 @@
+require 'sinatra'
+
+
+get '/' do
+  'Hello World'
+end
+
+get '/string-cheese' do
+  "Prince is the new mod instructor"
+end
--- 12-sinatra-intro/sinatra_app/app/controllers/application_controller.rb
+++ 12-sinatra-intro/sinatra_app/app/controllers/application_controller.rb
@@ -11,4 +11,32 @@ class ApplicationController < Sinatra::Base
   set :root, File.join(File.dirname(__FILE__), '..')
   set :views, File.join(root, "views")
 
+  # To see all persons/peoples
+  # Person.all
+
+  # REST -> Representational State Transfer
+  # A convention for showing "everything" of one model
+  # Index Action -> "Shows us everything of one model"
+  # "/<model name as plural>"
+  get "/people" do
+    @people = Person.all
+    erb :index
+  end
+
+
+  get "/people/new" do
+    "This will be where we will add a new form to create a person"
+  end
+
+  # Show Action -> Show us A SPECIFIC PERSON
+  get "/people/:id" do
+    @person = Person.find(params["id"])
+    erb :show
+    # binding.pry
+  end
+
+  get "/corgissssssssssssssss" do
+    "Wowowowowowowowow"
+  end
+
 end
