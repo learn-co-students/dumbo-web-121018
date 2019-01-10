5b16c1f80273219db97595b854c6d542b9e3ac73
Maxcell Wilson <maxcell.wilson@gmail.com>
2019-01-10 12:49:59 -0500

    Update to include building connection with genres

--- 15-sinatra-assoc-2/pairing/app/controllers/songs_controller.rb
+++ 15-sinatra-assoc-2/pairing/app/controllers/songs_controller.rb
@@ -3,17 +3,40 @@ class SongsController < ApplicationController
   # What controller action? New Action
   get "/songs/new" do
     @artists = Artist.all
+    @genres = Genre.all
     erb :new
   end
 
   # What controller action? Create Action
   post "/songs" do
     binding.pry
+
+    # Check out the params
+    # You'll notice the params has:
+    # name is a string but genres is an array
+    # {"name" => "Toxic", "genres" => [1,2,3]}
+    # we will iterate over the genres and add a connection
+    # for each one
+
+    song = Song.new
+    song.name = params[:name]
+    # we're setting the artist id here
+    song.artist_id = params[:artist_id]
+    # Now we'll iterate over and add each genre
+    params["genre_ids"].each do |genre_id|
+      song.genres << Genre.find(genre_id)
+      song.save
+    end
+
+
+
+    redirect "/songs/#{song.id}"
   end
 
   # What controller action do we go to AFTER we create? Show Action
   get "/songs/:id" do
     @song = Song.find(params[:id])
+    binding.pry
     erb :show
   end
 end
