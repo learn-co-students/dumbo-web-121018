ba657929451e3c507989e4fe09c841dfab27ddef
Maxcell Wilson <maxcell.wilson@gmail.com>
2019-01-10 11:03:34 -0500

    Updates to include previous work

---
+++ 15-sinatra-assoc-2/our-app/db/migrate/20190107164354_create_people.rb
@@ -0,0 +1,9 @@
+class CreatePeople < ActiveRecord::Migration[5.2]
+  def change
+    create_table :people do |t|
+      t.string :name
+      t.integer :age
+      t.string :pronouns
+    end
+  end
+end
