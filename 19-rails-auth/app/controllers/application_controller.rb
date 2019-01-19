ec0a129f76eeb7f77d75eccf55fc2c3d37156af9
Maxcell Wilson <maxcell.wilson@gmail.com>
2019-01-19 13:22:20 -0500

    Update to include README for Auth

--- 19-rails-auth/app/controllers/application_controller.rb
+++ 19-rails-auth/app/controllers/application_controller.rb
@@ -15,6 +15,8 @@ class ApplicationController < ActionController::Base
   end
 
   def current_user
-    session[:user_id]
+    if session[:user_id]
+      @current_user = User.find(session[:user_id])
+    end
   end
 end
