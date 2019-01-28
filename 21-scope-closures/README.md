ac999c85a2413b64dedd8af7dd1d5d7a41469f89
Graham Troyer-Joy <graham.troyer-joy@flatironschool.com>
2019-01-28 17:01:01 -0500

    scope

---
+++ 21-scope-closures/README.md
@@ -0,0 +1,20 @@
+# Key questions
+
+## Variables & Scope
+* What are the similarities and differences between var, let, and const
+* What are the differences between global, functional, and block scope?
+* If I forget how scope works, how can learn more about which of my variables are accessible where?
+
+## Closures
+* What is closure, and why is it helpful when passing a callback?
+
+## Objects
+* How can I access the values of object properties (keys) in JS?
+* What does it mean that objects are pass-by-reference in JS?
+
+## wat 
+* What is type coercion? Should I default to three equals signs or two when comparing values?
+* What's the difference between true and truthy? What things are truthy / falsy in JS?
+* How can I quickly test to see if a value is truthy or falsy if I forget?
+
+
---
+++ 21-scope-closures/index.js
@@ -0,0 +1,140 @@
+
+
+// //global
+
+// foo = "bar"
+// console.log(foo)
+
+
+// var potato = "fried";
+
+// console.log(potato)
+
+
+// function doStuff(){
+//   // function scope
+//   var potato = "split";
+//   console.log(foo)
+//   console.log(potato)
+//   debugger
+//   // console.log(banana)
+// }
+
+// // console.log(banana)
+
+// console.log(potato);
+
+// doStuff();
+
+// console.log(potato);
+
+
+
+
+
+// function nothingButLoop() {
+//   if (true) {
+//     let potato =  20
+//     // console.log(i)
+//   }
+//   // console.log(i);
+//   console.log(potato);
+// }
+
+
+// nothingButLoop();
+
+// // console.log(i);
+// console.log(potato);
+
+
+
+// const foo = "bar"
+
+
+// console.log(foo.toUpperCase())
+
+// console.log(foo)
+
+
+
+
+// const alphabet = ["a", "ba", "ceed", "eef", "gee"]
+
+// var capitalize = true
+
+// function callback(letter, makeTheLetterCapitalized){
+//   // console.log(makeTheLetterCapitalized)
+//   if (makeTheLetterCapitalized) {
+//     console.log(letter.toUpperCase())
+//   } else {
+//     console.log(letter)
+//   }
+// }
+
+// alphabet.forEach(function(letter){
+//   console.log(letter)
+//   callback(letter, capitalize)
+// })
+
+// const jane = {
+//   name: "Jane Costa",
+//   title: "Technical Coaching Fellow",
+//   skills: ["js", "empathy"], 
+//   employer: {
+//     name: "Flatiron School"
+//   }
+// }
+
+
+// function dancingClasses(person) {
+//   person.skills.push("dancing")
+// }
+
+
+
+// console.log(jane.skills)
+// dancingClasses(jane);
+// console.log(jane.skills)
+
+
+// function giveSillyLastName(personName) {
+//   personName + " agsfhdjkgaslkdjfhbasdkflhjbglruhiafnsdbhdgjdujk";
+//   console.log(personName)
+// }
+
+
+// var janeName = "Jane"
+
+
+// giveSillyLastName(janeName)
+
+
+// console.log(janeName)
+
+const instructor = 0
+
+if (instructor) {
+  console.log("it's zero!")
+} else {
+  console.log("swaadfkj")
+}
+
+if ([].length)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
