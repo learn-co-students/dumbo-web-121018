e2a00da5ef1f4f2bd37a1ef2e794cc3b788f7dbc
github username <github email address>
2019-03-04 11:13:53 -0500

    server source code

---
+++ 30-react/rap-rater-backend/app/controllers/application_controller.rb
@@ -0,0 +1,31 @@
+class ApplicationController < ActionController::API
+
+    def encode_token(payload)
+      JWT.encode(payload, 'secret')
+    end
+
+    def auth_header
+      request.headers['Authorization']
+    end
+
+    def decoded_token
+      if auth_header
+        begin
+          JWT.decode(auth_header, 'secret')
+        rescue JWT::DecodeError
+          nil
+        end
+      end
+    end
+
+    def current_user
+      if decoded_token
+        user_id = decoded_token[0]['user_id']
+        @user = User.find_by(id: user_id)
+      end
+    end
+
+    def logged_in?
+      !!current_user
+    end
+end
---
+++ 30-react/rap-rater-backend/config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+
+  namespace :api do
+    namespace :v1 do
+      resources :users, only: [:create]
+      post '/login', to: 'auth#create'
+    end
+  end
+end
